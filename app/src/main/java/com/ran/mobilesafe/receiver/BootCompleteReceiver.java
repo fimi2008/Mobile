38422f99e4d0235ff245798a4824096ce53af1e4
wangxiang <574693894@qq.com>
2015-10-20 14:02:43 +0800

    广播处理增加是否开启手机防盗判断

--- app/src/main/java/com/ran/mobilesafe/receiver/BootCompleteReceiver.java
+++ app/src/main/java/com/ran/mobilesafe/receiver/BootCompleteReceiver.java
@@ -11,7 +11,7 @@ import com.ran.mobilesafe.properties.ParameterUtils;
 
 /**
  * 监听手机开机启动的广播
- *
+ * <p/>
  * 作者: wangxiang on 15/10/19 16:43
  * 邮箱: vonshine15@163.com
  */
@@ -19,15 +19,19 @@ public class BootCompleteReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         SharedPreferences mPref = context.getSharedPreferences(ParameterUtils.SP_NAME, Context.MODE_PRIVATE);
-        String sim = mPref.getString("sim", null); // 获取绑定sim卡序列号
-        if (!TextUtils.isEmpty(sim)){
-            // 获取当前手机的sim卡
-            TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-            String currentSim = tm.getSimSerialNumber();
-            if (!sim.equals(currentSim)){
-                System.out.println("sim卡改变,发送报警短信");
-            }else{
-                System.out.println("手机安全");
+        boolean protect = mPref.getBoolean(ParameterUtils.PROTECT, false);
+        // 是否开启手机防盗
+        if (protect) {
+            String sim = mPref.getString("sim", null); // 获取绑定sim卡序列号
+            if (!TextUtils.isEmpty(sim)) {
+                // 获取当前手机的sim卡
+                TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+                String currentSim = tm.getSimSerialNumber();
+                if (!sim.equals(currentSim)) {
+                    System.out.println("sim卡改变,发送报警短信");
+                } else {
+                    System.out.println("手机安全");
+                }
             }
         }
     }
